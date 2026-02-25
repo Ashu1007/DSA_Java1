c6ab4f6585eec48ba3ec84d31acb269bcc32b4e1
Ashutosh Tripathi <tripathiashutosh665@gmail.com>
2026-02-26 00:38:32 +0530

    basic code

---
+++ src/basic_program/VolumeOfHemisphere.java
@@ -0,0 +1,9 @@
+package basic_program;
+
+public class VolumeOfHemisphere {
+    public static void main(String[] args) {
+        int r=3;
+
+        System.out.println((double)2/3*3.1415*r*r*r);
+    }
+}
---
+++ src/basic_program/VolumeOfSphere.java
@@ -0,0 +1,11 @@
+package basic_program;
+
+public class VolumeOfSphere {
+    public static void main(String[] args) {
+
+        int r=5;
+        double v=(double)4/3*3.1415*r*r*r; //also 4.0/3
+
+        System.out.println(v);
+    }
+}
