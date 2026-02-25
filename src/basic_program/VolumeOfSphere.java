0ea2d5111e1c29a5cbee0c2956d312b1761b3e97
Ashutosh Tripathi <tripathiashutosh665@gmail.com>
2026-02-26 00:51:29 +0530

    Scanner input

--- src/basic_program/VolumeOfSphere.java
+++ src/basic_program/VolumeOfSphere.java
@@ -1,11 +1,15 @@
 package basic_program;
 
+import java.util.Scanner;
+
 public class VolumeOfSphere {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the value: ");
 
-        int r=5;
+        int r= sc.nextInt();
         double v=(double)4/3*3.1415*r*r*r; //also 4.0/3
 
-        System.out.println(v);
+        System.out.println("Volume of Sphere: "+v);
     }
 }
